382622ba3be6393a105cfd6042b75441d0b43f87
VISHU9918 <vk9628117@gmail.com>
2024-07-11 01:18:08 +0530

    first commit

---
+++ src/Menu.js
@@ -0,0 +1,84 @@
+const Menu=[
+    {
+        id:1,
+        image:"images/maggi.jpg",
+        name:"maggi",
+        category:"breakfast",
+        price:"12₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:2,
+        image:"images/pakauda.jpg",
+        name:"allu pakauda",
+        category:"evening",
+        price:"20₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:3,
+        image:"images/corn.jpg",
+        name:"corn",
+        category:"breakfast",
+        price:"10₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:4,
+        image:"images/chhola.jpg",
+        name:"chola",
+        category:"lunch",
+        price:"50₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:5,
+        image:"images/pizza.jpg",
+        name:"pizza",
+        category:"evening",
+        price:"80₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:6,
+        image:"images/non-veg.jpg",
+        name:"Non-Veg Thali",
+        category:"dinner",
+        price:"180₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:7,
+        image:"images/sweets.jpg",
+        name:"Sweets",
+        category:"dinner",
+        price:"60₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:8,
+        image:"images/rajma-chawal.jpg",
+        name:"Rajma Rice",
+        category:"lunch",
+        price:"60₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+
+    {
+        id:9,
+        image:"images/samosha.jpg",
+        name:"samosa",
+        category:"evening",
+        price:"10₹",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+];
+
+export default Menu;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import GallaryReact from "./GallaryReact";
+
+ReactDom.render(
+<GallaryReact/>,
+document.getElementById("root")
+);
